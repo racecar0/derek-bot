14b58620181064a8894de43aaadf4ef0751f1338
Thomas <racecar1@gmail.com>
2020-02-05 16:21:43 -0700

    add status and special moves to rpg command

--- commands/rpg.js
+++ commands/rpg.js
@@ -15,16 +15,19 @@ rpg.fight = (commandMessage) => {
 		name: commandMessage.author.username,
 		hitPoints: 15,
 		damageMin: 1,
-		damageMax: 5
+		damageMax: 5,
+		shield: 2,
+		specialCount: 4
 	};
-	combat.setup(commandMessage, monster, player);
+	var turnCount = 0;
+	combat.setup(commandMessage, monster, player, turnCount);
 };
 
 module.exports = rpg;
 
 //Function Declarations
 var combat = {};
-combat.setup = function(commandMessage, monster, player) {
+combat.setup = function(commandMessage, monster, player, turnCount) {
 	commandMessage.channel
 		.send(
 			'Foe: ' +
@@ -38,10 +41,21 @@ combat.setup = function(commandMessage, monster, player) {
 		)
 		.then((sent) => {
 			// 'sent' is that message you just sent
-			sent.react('⚔').then(() => sent.react('🛡')).then(() => sent.react('🩹'));
+			sent
+				.react('⚔')
+				.then(() => sent.react('🛡'))
+				.then(() => sent.react('🩹'))
+				.then(() => sent.react('❔'))
+				.then(() => {
+					if (player.specialCount >= turnCount) {
+						sent.react('✨');
+					}
+				});
 
 			const filter = (reaction, user) => {
-				return [ '⚔', '🛡', '🩹' ].includes(reaction.emoji.name) && user.id === commandMessage.author.id;
+				return (
+					[ '⚔', '🛡', '🩹', '❔', '✨' ].includes(reaction.emoji.name) && user.id === commandMessage.author.id
+				);
 			};
 
 			sent
@@ -50,11 +64,15 @@ combat.setup = function(commandMessage, monster, player) {
 					const reaction = collected.first();
 
 					if (reaction.emoji.name === '⚔') {
-						combat.attack(commandMessage, monster, player);
+						combat.attack(commandMessage, monster, player, turnCount);
 					} else if (reaction.emoji.name === '🛡') {
-						combat.defend(commandMessage, monster, player);
+						combat.defend(commandMessage, monster, player, turnCount);
 					} else if (reaction.emoji.name === '🩹') {
-						combat.heal(commandMessage, monster, player);
+						combat.heal(commandMessage, monster, player, turnCount);
+					} else if (reaction.emoji.name === '❔') {
+						combat.status(commandMessage, monster, player, turnCount);
+					} else if (reaction.emoji.name === '✨') {
+						combat.special(commandMessage, monster, player, turnCount);
 					}
 				})
 				.catch((collected) => {
@@ -62,26 +80,27 @@ combat.setup = function(commandMessage, monster, player) {
 				});
 		});
 };
-combat.attack = function(commandMessage, monster, player) {
-	player.damage = Math.floor(Math.random() * (player.damageMax - player.damageMin) + player.damageMin);
+combat.attack = function(commandMessage, monster, player, turnCount) {
+	player.damage = Math.floor(Math.random() * player.damageMax + player.damageMin);
 	commandMessage.channel.send(
 		'```diff\n- ' + commandMessage.author.username + ' swings for ' + player.damage + ' damage.\n```'
 	);
 	monster.hitPoints -= player.damage;
 
-	monster.damage = Math.floor(Math.random() * (monster.damageMax - monster.damageMin) + monster.damageMin);
+	monster.damage = Math.floor(Math.random() * monster.damageMax + monster.damageMin);
 	commandMessage.channel.send('```diff\n- ' + monster.name + ' swings for ' + monster.damage + ' damage.\n```');
 	player.hitPoints -= monster.damage;
 
 	if (monster.hitPoints > 0 && player.hitPoints > 0) {
-		combat.setup(commandMessage, monster, player);
+		turnCount++;
+		combat.setup(commandMessage, monster, player, turnCount);
 	} else if (player.hitPoints <= 0) {
 		commandMessage.channel.send('You are dead.');
 	} else {
 		commandMessage.channel.send('Victory!');
 	}
 };
-combat.defend = function(commandMessage, monster, player) {
+combat.defend = function(commandMessage, monster, player, turnCount) {
 	monster.damage = Math.floor(Math.random() * (monster.damageMax - monster.damageMin) + monster.damageMin);
 	commandMessage.channel.send('```diff\n- ' + monster.name + ' swings for ' + monster.damage + ' damage.```');
 	setTimeout(() => {
@@ -89,7 +108,8 @@ combat.defend = function(commandMessage, monster, player) {
 	}, 250);
 	setTimeout(() => {
 		if (monster.hitPoints > 0 && player.hitPoints > 0) {
-			combat.setup(commandMessage, monster, player);
+			turnCount++;
+			combat.setup(commandMessage, monster, player, turnCount);
 		} else if (player.hitPoints <= 0) {
 			commandMessage.channel.send('You are dead.');
 		} else {
@@ -97,7 +117,7 @@ combat.defend = function(commandMessage, monster, player) {
 		}
 	}, 250);
 };
-combat.heal = function(commandMessage, monster, player) {
+combat.heal = function(commandMessage, monster, player, turnCount) {
 	player.damage = Math.floor(Math.random() * (player.damageMax - player.damageMin) + player.damageMin);
 	commandMessage.channel.send(
 		'```diff\n+ ' + commandMessage.author.username + ' heals for ' + player.damage + ' damage.```'
@@ -109,7 +129,45 @@ combat.heal = function(commandMessage, monster, player) {
 	player.hitPoints -= monster.damage;
 
 	if (monster.hitPoints > 0 && player.hitPoints > 0) {
-		combat.setup(commandMessage, monster, player);
+		turnCount++;
+		combat.setup(commandMessage, monster, player, turnCount);
+	} else if (player.hitPoints <= 0) {
+		commandMessage.channel.send('You are dead.');
+	} else {
+		commandMessage.channel.send('Victory!');
+	}
+};
+combat.status = function(commandMessage, monster, player, turnCount) {
+	commandMessage.channel.send(
+		'Attack Damage: ' +
+			player.damageMin +
+			'-' +
+			player.damageMax +
+			' damage' +
+			'\nShield Rating: ' +
+			player.shield +
+			'\nTurns until Special: ' +
+			(player.specialCount - turnCount)
+	);
+
+	combat.setup(commandMessage, monster, player, turnCount);
+};
+combat.special = function(commandMessage, monster, player, turnCount) {
+	commandMessage.channel.send('```http\n' + monster.name + ' can only stand in awe of your special attack!\n```');
+
+	player.damage = Math.floor(Math.random() * player.damageMax + player.damageMin);
+	commandMessage.channel.send(
+		'```css\n' +
+			commandMessage.author.username +
+			' pretends like they know what they are doing for ' +
+			player.damageMax +
+			' damage!!!\n```'
+	);
+	monster.hitPoints -= player.damage;
+	turnCount = 0;
+
+	if (monster.hitPoints > 0 && player.hitPoints > 0) {
+		combat.setup(commandMessage, monster, player, turnCount);
 	} else if (player.hitPoints <= 0) {
 		commandMessage.channel.send('You are dead.');
 	} else {
